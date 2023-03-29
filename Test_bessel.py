a9307478f6da7d398feba77950659ea2f0703a67
Jim <jnmerk@gmail.com>
2023-03-29 16:14:38 -0400

    Add files via upload

--- Test_bessel.py
+++ Test_bessel.py
@@ -3,15 +3,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.signal as signal
 
-mydata = [[000 for x in range(2100)] for y in range(2101)]  # col by row
+my_data = [[000 for x in range(2100)] for y in range(2101)]  # col by row
+filter_data=[[000 for x in range(2100)] for y in range(2101)]
 count =1
 jcount=1
 
-#b,a=signal.bessel(2,1,btype='Low',norm='Mag',fs=2500)  
+b,a=signal.bessel(2,1,btype='LOw',norm='Mag',fs=2500)
+print (b,a)
+
+output=0 
 
 with open("1 run.txt", mode='r', encoding='utf-8' ) as f:
 
-    graph_label = "Test Bessel filter"
+    graph_label = "Raw Signal"
     line = f.readline()
     while line:
         line = f.readline()
@@ -20,19 +24,22 @@ with open("1 run.txt", mode='r', encoding='utf-8' ) as f:
             line = 0
         if line !="":
             test = int(line)
-            print("this is sample # ", count, test)
-            (mydata[count][jcount])=test 
+            #print("this is sample # ", count, test)
+            (my_data[count][jcount])=test 
             count=count+1  ## stored a good time value, so increment the counter
-          
-        
-my_data=np.array(mydata)
+            
+my_data=np.array(my_data)  
+filter_data = signal.lfilter(b, a,my_data)
+#print("this is filtered sample # ", count, output)
 
 
 mpl.rcParams['figure.figsize'] = [12, 6]
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(2)
 plt.title(graph_label)
-
-ax.plot(my_data[1:1000,1:2])     # plotting only the first 1000 ADC data points for the first run only 
-                                                     # there is a total of 2100 ADC voltage vs time points for 61 runs in the data file - only plotting a subset
-plt.savefig("first 10 runs only.png")
+ax[0].plot(my_data[1:1000,1:2])
+ax[1].plot(filter_data[1:1000,1:2])
+plt.savefig ("Bessel.png")# plotting only the first 1000 ADC data points for the first run only 
+                                                  # there is a total of 2100 ADC voltage vs time points for 61 runs in the data file - only plotting a subset
 plt.show()
+
+     
